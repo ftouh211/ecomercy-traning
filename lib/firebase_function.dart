f41b074a48571af6ec5894b8c0cec5f2b9f10b82
Dell <ftou7abdo21@gmail.com>
2024-08-27 04:51:30 +0300

    eneeed

--- lib/firebase_function.dart
+++ lib/firebase_function.dart
@@ -1,39 +1,3 @@
-// import 'package:firebase_auth/firebase_auth.dart';
-//
-// class FirebaseFunctions{
-//  static login(String emailAddress,String password) async {
-//     try {
-//       final credential = await FirebaseAuth.instance.signInWithEmailAndPassword(
-//           email: emailAddress,
-//           password: password
-//       );
-//     } on FirebaseAuthException catch (e) {
-//       if (e.code == 'user-not-found') {
-//         print('No user found for that email.');
-//       } else if (e.code == 'wrong-password') {
-//         print('Wrong password provided for that user.');
-//       }
-//     }
-//   }
-//  static createAcount(String email, String password) async{
-//     try {
-//       final credential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
-//         email: email,
-//         password: password,
-//       );
-//       credential.user?.sendEmailVerification();
-//
-//     } on FirebaseAuthException catch (e) {
-//       if (e.code == 'weak-password') {
-//         print('The password provided is too weak.');
-//       } else if (e.code == 'email-already-in-use') {
-//         print('The account already exists for that email.');
-//       }
-//     } catch (e) {
-//       print(e);
-//     }
-//   }
-// }
 
 
 
